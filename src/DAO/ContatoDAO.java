88efde4ba8370e0772d047a32f0f2473a420b511
Jorge Junior <junior-soares42@hotmail.com>
2019-04-08 13:15:26 -0300

    ContatoDAO

---
+++ src/DAO/ContatoDAO.java
@@ -0,0 +1,35 @@
+package DAO;
+
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import javax.swing.JOptionPane;
+
+import BEANS.ContatoBEAN;
+
+public class ContatoDAO {
+	private ContatoBEAN contato;
+
+	Conexao conecta = new Conexao();
+    public ContatoDAO(){
+        conecta.conexao();    
+    }
+    PreparedStatement pst;
+    
+	
+	private void inserirContato(ContatoBEAN cliente) {//GEN-FIRST:event_numeroKeyPressed
+            try {
+            pst = conecta.conn.prepareStatement("insert into Cliente(Nome, Telefone, Numero, Bairro, Rua) values (?,?,?,?,?)");
+            pst.setString(1, contato.getNome());
+            pst.setString(2, contato.getTelefone());
+            pst.setInt(3, contato.getNumero());
+            pst.setString(4, contato.getBairro());
+            pst.setString(5, contato.getRua());
+            pst.executeUpdate();
+            JOptionPane.showMessageDialog(null, "Inserido com sucesso!");
+
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Não foi inserido!" );
+        }
+        }
+    }
